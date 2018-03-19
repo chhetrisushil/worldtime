617da2616384c916c683310705bffdb8c0e89292
Sushil Chhetri <chhetrisushil@gmail.com>
2018-03-19 23:07:31 +0530

    Token
    - Adding HTTPOnly cookie
    - Cookie perser middleware

--- server/app.js
+++ server/app.js
@@ -2,12 +2,14 @@
 var express = require('express');
 var app = express();
 var cors = require('cors');
+var cookieParser = require('cookie-parser');
 var UserController = require('./controller/userController');
 var ZoneController = require('./controller/zoneController');
 
 // initialize db
 require('./db');
 
+app.use(cookieParser());
 app.use(cors());
 app.use('/users', UserController);
 app.use('/zones', ZoneController);
--- server/authenticate.js
+++ server/authenticate.js
@@ -1,9 +1,30 @@
+const jwt = require('jsonwebtoken');
+const config = require('./config/default.json');
+
 function authenticate(req, res, next) {
-  if (req.path === '/login'/* req.authorised */) {
-    next();
-  } else {
-    res.status(401).send(`Unauthorised Access ${req.path}`);
+  try {
+    console.log(`token from cookie: ${req.cookies.token}`);
+    if (req.path === '/login') {
+      return next();
+    }
+
+    const token = req.headers['x-access-token'];
+    const isValid = jwt.verify(token, config.secret);
+
+    const cookie = req.cookies.token;
+
+    console.log(cookie);
+
+    if (isValid) {
+      next();
+    } else {
+      res.status(401).send(`Unauthorised Access to ${req.path}`);
+    }
+
+  } catch (e) {
+    res.status(500).send(`Internal Server Error: ${e.message}`);
   }
+
 }
 
 module.exports = authenticate;
